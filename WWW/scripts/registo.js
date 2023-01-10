c2a29ab620a63451598bd54e2a96f3e3c6c347a2
Gustavo Manteigas <gustavoluzmanteigas@gmail.com>
2023-01-10 18:20:27 +0000

    registo_user.v1

--- WWW/scripts/registo.js
+++ WWW/scripts/registo.js
@@ -1,28 +1,119 @@
 /**
  * Função chamada quando página é carregada
  */
-function init(){
+function init() {
 
     var btn_empre = document.getElementById("btn_empre"); //Botão de registo da tab de empresas
     var btn_profi = document.getElementById("btn_profi"); //Botão de registo da tab de profissionais
     var div_emp = document.getElementById("reg_emp"); //DIV com a informação para registar uma empresa
     var div_pro = document.getElementById("reg_pro");//DIV com a informação para registar um profissional
-    
-    btn_empre.addEventListener("click", function(evt){
-        btn_empre.style.color ="#123370"; //Azul
-        btn_profi.style.color ="#aaaaaa"; //Cinzento
+
+    btn_empre.addEventListener("click", function (evt) {
+        btn_empre.style.color = "#123370"; //Azul
+        btn_profi.style.color = "#aaaaaa"; //Cinzento
         div_emp.style.display = "initial";
         div_pro.style.display = "none";
     });
-    
-    btn_profi.addEventListener("click", function(evt){
-        btn_profi.style.color ="#123370"; //Azul
-        btn_empre.style.color ="#aaaaaa"; //Cinzento
+
+    btn_profi.addEventListener("click", function (evt) {
+        btn_profi.style.color = "#123370"; //Azul
+        btn_empre.style.color = "#aaaaaa"; //Cinzento
         div_emp.style.display = "none";
         div_pro.style.display = "initial";
     });
-    
+}
+
+var user;
+
+//Função que regista um novo profissional
+function newUserPro() {
+    var gen = document.getElementById('genero_pro');
+    var visible = document.getElementById('visiblePro');
+    var vis = 0;
+
+    //Transforma dados para não dar erro na BD
+    if (visible.checked == true) {
+        vis = 1;
     }
-    
-    
-    window.onload = init;
+
+//document.getElementById('desc_pro').value,
+
+    var data = {
+        Nome: document.getElementById('nome_pro').value,
+        Email: document.getElementById('email_pro').value,
+        Password: document.getElementById('password_pro').value,
+        DataNas: document.getElementById('dataNasc_pro').value,
+        Localidade: document.getElementById('local_pro').value,
+        Genero: gen.options[gen.selectedIndex].text,
+        Descricao: "Teste de descrição",
+        isVisible: vis
+    };
+    console.log(data);
+
+    var url = "/newUser/" + 1;
+
+    var xhttp = new XMLHttpRequest();
+    //Open first, before setting the request headers.
+    xhttp.open("POST", url, true);
+    xhttp.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            //Ir buscar o user que foi criado
+            getUser(data);
+        }
+    };
+
+    xhttp.send(JSON.stringify(data));
+}
+
+function createPro(dataPro) {
+    console.log(dataPro);
+
+    var url = "/reg_pro";
+
+    var xhttp2 = new XMLHttpRequest();
+    //Open first, before setting the request headers.
+    xhttp2.open("POST", url, true);
+    xhttp2.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+    xhttp2.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            alert('Utilizador criado');
+            //Limpar as caixas de texto todas
+            document.forms["reg_profissional"].reset();
+        }
+    }
+    xhttp2.send(JSON.stringify(dataPro));
+}
+
+function getUser(data) {
+    // Criar a instância de XMLHttpRequest
+    if (window.XMLHttpRequest) {
+        xhr = new XMLHttpRequest();
+    } else {
+        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    if (xhr) {
+        let url = "http://127.0.0.1:5502/getUser/" + data.Email;
+        // Configurar a solicitação
+        xhr.open('GET', url, true);
+
+        // Definir a função de retorno de chamada
+        xhr.onreadystatechange = function () {
+            if ((xhr.readyState === 4) && (xhr.status === 200)) {
+                // Fazer algo com os dados recebidos
+                var resultado = JSON.parse(xhr.responseText);
+                var linhaResultado = resultado[0];
+
+                data.IdUser = linhaResultado["idUser"];
+
+                createPro(data);
+            }
+        };
+    }
+
+    // Enviar a solicitação
+    xhr.send();
+}
+
+window.onload = init;
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,7 +1,72 @@
+const { Console } = require("console");
 const fs = require("fs");
 const connection = require("../config/connection")
 
 module.exports = function (app) {
+
+    app.post('/reg_emp', function (req, res) {
+        //Registo das empresas
+       
+    });
+
+    //Cria novo profissional
+    app.post('/reg_pro', function (req, res) {
+        console.log("sucesso");
+        //Registo dos profissionais
+        let info = [[req.body.IdUser,req.body.Nome,req.body.Email,req.body.Password,req.body.DataNas,req.body.Localidade, req.body.Genero, req.body.Descricao, req.body.isVisible]];
+        console.log(req.body);
+        let query = "INSERT INTO Profissionais (Users_idUser,nome,email,password,dataNasc,localidade,genero,descricao,isVisible) VALUES ?";
+        
+        console.log("INFO " + info);
+        var delayInMilliseconds = 1000; //1 second
+
+        setTimeout(function() {
+            connection.query(query,[info], function(err, data){
+                if(err){
+                    console.log("Error inserting : %s ",err );
+                }
+                else{
+                    console.log(data);
+                    res.status(200).send();
+                }	
+            });
+        }, delayInMilliseconds);
+    });
+
+    //Criar novo user
+    app.post('/newUser/:idTipo', function (req, res) {
+        let info = [[req.params.idTipo, '2022-10-13', req.body.Email]];
+        console.log(info);
+        let query = "INSERT INTO Users (TipoUser_idTipoUser,dataAdesao,email) VALUES ?";
+        console.log("Query de insert: " + query);
+
+        connection.query(query,[info], function (error, data) {
+            if (error) {
+                res.render(error)
+            }
+            else {
+                console.log(data);
+                res.status(200).send();
+            }
+        });
+    });
+
+    app.get('/getUser/:email', function (req, res) {
+        console.log(req.params.email);
+        //let info = [[req.params.email]];
+
+        let query = "select * from Users where email = ?";
+
+        connection.query(query,req.params.email, function (error, results, fields) {
+            if (error) {
+                res.render(error)
+            }
+            else {
+                console.log(results);
+                res.end(JSON.stringify(results));
+            }
+        });
+    });
 
     //rest api para obter ofertas.
     app.get('/ofertas', function (req, res) {
